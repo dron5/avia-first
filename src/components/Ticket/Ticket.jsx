9c3e5387577620b372012b6fe1b4038d519f5b5e
dron5 <dron5@mail.ru>
2021-03-12 13:56:07 +0700

    TicketSegment component added3

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 
-// import TicketSegment from './TicketSegment';
+import TicketSegment from '../TicketSegment';
 import classes from "./Ticket.module.scss";
 
 const Ticket = ({ price, carrier, segments }) => {
@@ -17,8 +17,6 @@ const Ticket = ({ price, carrier, segments }) => {
   )
     .toTimeString()
     .slice(0, 5);
-  const stopsTo = segments[0].stops.join(" ");
-  const stopsFrom = segments[1].stops.join(" ");
   const stops = (args) => {
     switch (args) {
       case 1:
@@ -31,8 +29,6 @@ const Ticket = ({ price, carrier, segments }) => {
         return "0 ПЕРЕСАДОК";
     }
   };
-  const countStopsTo = stops(segments[0].stops.length);
-  const countStopsBack = stops(segments[1].stops.length);
   return (
     <div className={classes.Ticket}>
       <div className={classes.Ticket__price}>
@@ -41,52 +37,24 @@ const Ticket = ({ price, carrier, segments }) => {
           <img src={`http://pics.avs.io/99/36/${carrier}.png`} alt="LogoAvia" />
         </span>
       </div>
-      {/* <TicketSegment
-
-      /> */}
-      <div className={classes.Ticket__direction}>
-        <div className={classes.Ticket__part}>
-          <div className={classes.title}>
-            {segments[0].origin} - {segments[0].destination}
-          </div>
-          <div className={classes.Ticket__info}>
-            {originTimeTo} - {destinationTimeTo}
-          </div>
-        </div>
-        <div className={classes.Ticket__part}>
-          <div className={classes.title}>В ПУТИ</div>
-          <div className={classes.Ticket__info}>
-            {Math.floor(segments[0].duration / 60)}ч{segments[0].duration % 60}м
-          </div>
-        </div>
-        <div className={classes.Ticket__part}>
-          <div className={classes.title}>{countStopsTo}</div>
-          <div className={classes.Ticket__info}>{stopsTo}</div>
-        </div>
-      </div>
-      {/* <TicketSegment
-        
-      /> */}
-      <div className={classes.Ticket__direction}>
-        <div className={classes.Ticket__part}>
-          <div className={classes.title}>
-            {segments[1].origin} - {segments[1].destination}
-          </div>
-          <div className={classes.Ticket__info}>
-            {originTimeBack} - {destinationTimeBack}
-          </div>
-        </div>
-        <div className={classes.Ticket__part}>
-          <div className={classes.title}>В ПУТИ</div>
-          <div className={classes.Ticket__info}>
-            {Math.floor(segments[1].duration / 60)}ч{segments[1].duration % 60}м
-          </div>
-        </div>
-        <div className={classes.Ticket__part}>
-          <div className={classes.title}>{countStopsBack}</div>
-          <div className={classes.Ticket__info}>{stopsFrom}</div>
-        </div>
-      </div>
+      <TicketSegment
+        countStops={stops(segments[0].stops.length)}
+        duration={segments[0].duration}
+        stops={segments[0].stops.join(" ")}
+        origin={segments[0].origin}
+        originTime={originTimeTo}
+        destinationTime={destinationTimeTo}
+        destination={segments[0].destination}
+      />
+      <TicketSegment
+       countStops={stops(segments[1].stops.length)}
+       duration={segments[1].duration}
+       stops={segments[1].stops.join(" ")}
+       origin={segments[1].origin}
+       originTime={originTimeBack}
+       destinationTime={destinationTimeBack}
+       destination={segments[1].destination} 
+      />
     </div>
   );
 };
--- src/components/TicketSegment/TicketSegment.jsx
+++ src/components/TicketSegment/TicketSegment.jsx
@@ -1,27 +1,29 @@
+/* eslint-disable */
 import React from 'react';
 
 import classes from './TicketSegment.module.scss';
 
-const TicketSegment = () => (
+const TicketSegment = ({ originTime, duration, origin,
+   destination, destinationTime, countStops, stops }) => (
   <div className={classes.Ticket__direction}>
-        {/* <div className={classes.Ticket__part}>
+        <div className={classes.Ticket__part}>
           <div className={classes.title}>
-            {segments[0].origin} - {segments[0].destination}
+            {origin} - {destination}
           </div>
           <div className={classes.Ticket__info}>
-            {originTimeTo} - {destinationTimeTo}
+            {originTime} - {destinationTime}
           </div>
         </div>
         <div className={classes.Ticket__part}>
           <div className={classes.title}>В ПУТИ</div>
           <div className={classes.Ticket__info}>
-            {Math.floor(segments[0].duration / 60)}ч{segments[0].duration % 60}м
+            {Math.floor(duration / 60)}ч{duration % 60}м
           </div>
         </div>
         <div className={classes.Ticket__part}>
-          <div className={classes.title}>{countStopsTo}</div>
-          <div className={classes.Ticket__info}>{stopsTo}</div>
-        </div> */}
+          <div className={classes.title}>{countStops}</div>
+          <div className={classes.Ticket__info}>{stops}</div>
+        </div>
       </div>
 );
 
