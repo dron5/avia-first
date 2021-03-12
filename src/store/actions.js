63ec7a3ce1a021a4c754af147ee2bbf980cb2156
dron5 <dron5@mail.ru>
2021-03-12 17:10:56 +0700

    stopsFilter added to action2

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,25 +1,11 @@
 import { fetchTickets, fetchSearchId } from "../asyncActions/fetchStuff";
-import { ticketFilter } from "../tools/actionFilter";
+import { getFiltered } from "../tools/actionFilter";
 
 export const onClick = (name) => ({ type: name.target.name });
 
 export const addSearchId = () => async (dispatch) => {
   const response = await fetchSearchId();
   dispatch({ type: "ADD_SEARCH_ID", payload: response });
-};
-
-const getFiltered = (tickets) => {
-  const data = {};
-  data.all = tickets;
-  data.none = tickets.filter(
-    (ticket) =>
-      ticket.segments[0].stops.length === 0 &&
-      ticket.segments[1].stops.length === 0
-  );
-  data.one = tickets.filter(ticketFilter(1));
-  data.two = tickets.filter(ticketFilter(2));
-  data.three = tickets.filter(ticketFilter(3));
-  return data;
 };
 
 export const addTickets = (id) => async (dispatch) => {
--- src/tools/actionFilter.js
+++ src/tools/actionFilter.js
@@ -1,8 +1,22 @@
-export const ticketFilter = (digit) => (ticket) => (
+const ticketFilter = (digit) => (ticket) => (
   (ticket.segments[0].stops.length === digit &&
     ticket.segments[1].stops.length < digit) ||
   (ticket.segments[1].stops.length === digit &&
     ticket.segments[0].stops.length < digit)
 );
 
-export default ticketFilter;
+const getFiltered = (tickets) => {
+  const data = {};
+  data.all = tickets;
+  data.none = tickets.filter(
+    (ticket) =>
+      ticket.segments[0].stops.length === 0 &&
+      ticket.segments[1].stops.length === 0
+  );
+  data.one = tickets.filter(ticketFilter(1));
+  data.two = tickets.filter(ticketFilter(2));
+  data.three = tickets.filter(ticketFilter(3));
+  return data;
+};
+
+export default getFiltered;
